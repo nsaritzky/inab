11cde49da5c47fe2dfe94d0341e0ad38549c1229
nsaritzky <nsaritzky@gmail.com>
2023-07-24 11:55:00 -0700

    Switch solid-select styling to internal CSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,31 @@
+const plugin = require("tailwindcss/plugin")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@kobalte/tailwindcss"), require("tailwindcss-animate")],
-};
+  plugins: [
+    require("@kobalte/tailwindcss"),
+    require("tailwindcss-animate"),
+    plugin(({ addVariant, e }) => {
+      addVariant("data-focused", `&[data-focused="true"]`)
+      addVariant("data-disabled", `&[data-disabled="true"]`)
+      addVariant("data-has-value", `&[data-has-value="true"]`)
+      addVariant("data-is-active", `&[data-is-active="true"]`)
+      addVariant("data-multiple", `&[data-multiple="true"]`)
+      addVariant("mark", "& > mark")
+    }),
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        ".text-inherit": {
+          font: "inherit",
+        },
+        ".outline-zero": {
+          outline: "none",
+        },
+      })
+    }),
+  ],
+}
