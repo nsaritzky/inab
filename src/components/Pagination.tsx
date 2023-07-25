3595f15bb64472fc7de2c1ee708423a824a14f98
nsaritzky <nsaritzky@gmail.com>
2023-07-25 10:14:02 -0700

    Added basic styling to pagination component

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,9 +13,22 @@ const Pagination = (props: PaginationProps): JSX.Element => {
   }
 
   return (
-    <ul>
+    <ul class="flex">
       <For each={[...Array(props.totalPages).keys()].map((i) => i + 1)}>
-        {(i) => <li onClick={() => props.onPageChange(i)}>{i}</li>}
+        {(i) => (
+          <li>
+            <button
+              class={`border border-blue-300 px-2 py-1 ${
+                i === props.currentPage
+                  ? "border-blue-500 bg-blue-500 text-white"
+                  : "bg-gray-100"
+              }`}
+              onClick={() => props.onPageChange(i)}
+            >
+              {i}
+            </button>
+          </li>
+        )}
       </For>
     </ul>
   )
